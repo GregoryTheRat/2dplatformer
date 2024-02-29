f6cd3a61e26a7a5fb000b434c66407d326a15239
GregoryTheRat <bankigergely0519@gmail.com>
2024-02-29 11:29:50 +0100

    fix

---
+++ out/build/CMakeFiles/2DPLATFORMER.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "2DPLATFORMER"
+  "2DPLATFORMER.pdb"
+  "CMakeFiles/2DPLATFORMER.dir/main.cpp.o"
+  "CMakeFiles/2DPLATFORMER.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2DPLATFORMER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
